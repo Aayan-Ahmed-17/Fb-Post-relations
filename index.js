c3c818469b7a84d9bf61b842e22516320d950b20
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-05 10:56:54 +0500

    connect db func removed

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ dotenv.config();
 import express from "express"
 import cors from "cors"
 import connectDB from "./src/db/index.js";
-import courseRoutes from "./src/routes/course.routes.js"
-import studentRoutes from "./src/routes/student.routes.js"
+
 
 
 const app = express()
-
+const port = 3000
 
 app.use(express.json())
 app.use(cors())
@@ -18,15 +17,19 @@ app.get('/', (req, res) => {
 })
 
 
-app.use('/api/v1', courseRoutes)
-app.use('/api/v1', studentRoutes)
+// app.use('/api/v1', courseRoutes)
+// app.use('/api/v1', studentRoutes)
 
-connectDB()
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MONGO DB connection failed !!! ", err);
-    });
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT, () => {
+//             console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log("MONGO DB connection failed !!! ", err);
+//     });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
